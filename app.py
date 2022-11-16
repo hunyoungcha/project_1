3a2b100599a18075f93385ffe66677f2514acd06
hunyoungcha <chamgf5247@naver.com>
2022-11-16 23:58:41 +0900

    project

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-#welcome 페이지 넘어갈때 id pw 가져가는 법
-#welcome 페이지에 welcome{id} 형식 만들기
-
+#폼 데이터를 submit 하면 웹페이지 못찾고 있음 <== 해결해야함
+#로그인 구현 되면 임시 디비 만들어서 거기에 저장해둘것
+#로그인 성공 실패 페이지 만들어서 성공하면 홈페이지, 실패하면 다시 시도
 
 
 from flask import Flask, render_template, request
-import os
-import database
+
 
 app = Flask(__name__)
 
@@ -14,17 +13,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET','POST'])
 def login():
-    id=request.form['id']
-    pw=request.form['pw']
-    
-    if request.method=='GET':
-        render_template('fail_login.html')
-    elif request.method=='POST':
-        if database.getKey(id) and database.getValue(pw) :
-            return render_template('welcome.html', id=id, pw=pw,)
-        else:
-            return render_template('fail_login.html')
+    id=request.form.get('id')
+    pw=request.form.get('pw')
+    return id, pw
+
 
 app.run()
