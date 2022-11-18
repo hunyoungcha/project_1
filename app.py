c31048a5c83848564415d883e0ba90483317654d
hunyoungcha <chamgf5247@naver.com>
2022-11-18 14:06:28 +0900

    ex

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #로그인 성공 실패 페이지 만들어서 성공하면 홈페이지, 실패하면 다시 시도
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 import os
 
 
@@ -17,5 +17,22 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/home/')
+def home():
+    return render_template('home.html')
 
-app.run(debug='on')
+@app.route('/login_confirm/', methods=['POST'])
+def login_confirm():
+    id= request.form['id']
+    pw= request.form['pw']
+    if (id=='admin' and pw=='admin') or (id=='guest' and pw=='guest'):
+        session['id']=id
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000) 
