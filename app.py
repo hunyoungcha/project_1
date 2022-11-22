afa8e66425892430aa11d3b15c99174c856672fc
hunyoungcha <chamgf5247@naver.com>
2022-11-22 15:12:40 +0900

    mail_success

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#메일 오류 나는거 고쳐야 함
+#execpt 부분 alert로 실패 알림 하고 다시 render_template('/')
 
 
 from flask import Flask, render_template, request, session, url_for, redirect
@@ -9,27 +9,32 @@ app = Flask(__name__)
 
 
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config["MAIL_PORT"] = 465
-app.config['MAIL_USERNAME'] = 'hunyoung52472@gmail.com'
-app.config['MAIL_PASSWORD'] = ''
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+app.config['MAIL_SERVER'] = 'smtp.naver.com'
+app.config["MAIL_PORT"] = 465
+app.config['MAIL_USERNAME'] = 'chamgf5247'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 
 @app.route('/mail/', methods=['GET', 'POST'])
 def mail():
-    address=request.values.get('emailAddress')
-    msg = Message('Hello', sender='hunyoung5247@gmail.com', recipients=[address])
-    msg.body = 'Hello Flask message sent fro Flask-Mail'
-    Mail(app).send(msg)
-    return redirect('/')
+    try:
+        address=request.values.get('emailAddress')
+        address=request.values.get('emailAddress')
+        msg = Message('Hello', sender='chamgf5247@naver.com', recipients=[address])
+        msg.body = 'Hello Flask message sent fro Flask-Mail'
+        Mail(app).send(msg)
+        return redirect('/')
+    except:
+        return redirect('/#signup')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
