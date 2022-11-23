ea0443ba1245e9911ae655e1fc73b4776325dd1a
hunyoungcha <chamgf5247@naver.com>
2022-11-23 14:43:19 +0900

    8

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #execpt 부분 alert로 실패 알림 하고 다시 render_template('/')
-
+#signup, login name 값 제대로 주기
 
 from flask import Flask, render_template, request, session, url_for, redirect, flash
 from flask_mail import Mail, Message
@@ -18,19 +18,33 @@ def index():
 app.config['MAIL_SERVER'] = 'smtp.naver.com'
 app.config["MAIL_PORT"] = 465
 app.config['MAIL_USERNAME'] = 'chamgf5247'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = 'Navergnsdud6361@'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
-@app.route('/check_mail/', methods=['GET', 'POST'])
-def mail():
-    try:
-        address=request.values.get('emailAddress')
-        return redirect('/right_email/')
+@app.route('/process_login/')
+def process_login():
+    email=request.values.get('email')
+    pw=request.values.get('pw')
 
-    except:
-        return 
+@app.route('/signup/')
+def signupk():
+    return render_template('signup.html')
+
+
+
+# @app.route('/check_mail/', methods=['GET', 'POST'])
+# def mail():
+#     try:
+#         address=request.values.get('emailAddress')
+#         return redirect('/right_email/')
+
+#     except:
+#         return 
 
 #         address=request.values.get('emailAddress')
 #         address=request.values.get('emailAddress')
